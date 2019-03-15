8b8dda8fc2879e5fb0dcc099bf5e4f6b2361c91a
Nataliya Revutska <nrevu@softserveinc.com>
2019-03-15 12:48:46 +0200

    Added work with wait helpers

--- UnitTest/Tests/BlackBoxNavigation.cs
+++ UnitTest/Tests/BlackBoxNavigation.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,13 @@ namespace SetupEnvironmentTest.Tests
     {
         static IWebDriver _driver;
         static string _url = "http://testing.todvachev.com/special-elements/radio-button-test/";
+
+        private void takeScreenshot()
+        {
+            ITakesScreenshot takeScreenshot = (ITakesScreenshot)_driver;
+            Screenshot screenshot = takeScreenshot.GetScreenshot();
+            screenshot.SaveAsFile("C:\\Users\\nrevu\\Pictures\\Saved Pictures\\screen1.png", ScreenshotImageFormat.Png);
+        }
 
         [OneTimeSetUp]
         public void ClassSetUp()
@@ -33,12 +41,25 @@ namespace SetupEnvironmentTest.Tests
         public void TestMethod()
         {
             IWebElement position = _driver.FindElement(By.Id("comment"));
-            Actions action = new Actions(_driver);
+            Actions action = new Actions(_driver); 
             action.MoveToElement(position).Perform();
-            // action.ClickAndHold().MoveToElement(position).Perform();
+             // action.ClickAndHold().MoveToElement(position).Perform();
 
             //IJavaScriptExecutor javaScript = (IJavaScriptExecutor)_driver;
             // javaScript.ExecuteScript("arguments[0].scrollIntoView(true);", position);
+           takeScreenshot();
+
+            // expected conditions is deprecated//
+            //ExpectedConditions.StalenessOf(position);
+            //
+            //
+            //instead of this 
+            //Using nuget, search for DotNetSeleniumExtras.WaitHelpers, import that namespace into your class
+
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+            position = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("comment")));
+
+
             Thread.Sleep(3000);
         }
 
